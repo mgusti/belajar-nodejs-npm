5af706b10c8475f9973ae2cb7f88b5f9f2c87c83
mgusti <mahkluk.astral@gmail.com>
2023-07-20 21:36:47 +0700

    npm

---
+++ package.js
@@ -0,0 +1,9 @@
+import { sayHello, sum } from "belajar-nodejs-npm-library-serenity";
+import { max, min } from "belajar-nodejs-npm-library-serenity/number";
+
+console.info(sayHello("Serenity"));
+
+console.info(sum([10, 10, 10, 10]));
+
+console.info(min(20, 30));
+console.info(max(20, 30));
